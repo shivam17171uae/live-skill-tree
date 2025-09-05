c2869b786008a947d2b218310ced66a97db44414
shivam17171uae <shivam17171uae@gmail.com>
2025-09-06 02:25:15 +0530

    fix

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Read the variables from the environment
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// Paste your own Supabase URL and anon key below
+const supabaseUrl = 'https://cegczrwjalpzmwnrsaak.supabase.co';
+const supabaseAnonKey = 'sb_publishable_xZWhBkKNbOS8mxzPD9mOkw_NFnIqZhR';
 
+// This creates the connection and exports it for our app to use
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
